45dc1daa0fc55f31f9f45bfcaf9997fa3f5b7250
StudioMaX <starosekpd@gmail.com>
2017-05-30 23:55:35 +0600

    New feature

--- test.php
+++ test.php
@@ -1,3 +1,14 @@
 <?php
 echo 'test content';
-echo 'second line';
+echo 'second line1111233111';
+echo 'second line1111233111 gfdggfdgdfg dgffdgdf gdf gdfgfdgfd gdf gfdg fd gfdg fdg fdg fd gdfg fdg fdg fg dfg fg dfg fdg fdg fdg fdg dfg dfgdf';
+
+/**
+ * @param string $obj
+ */
+function TsuperFunc(Obj1 $obj) {
+
+}
+$tt = new sdfasfdsffsdfsfsdfsd();
+$t = 5 / 0;
+gdfgdfgdf 43534 --gf gdf

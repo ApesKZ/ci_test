4475ecac4fb47078c40bc75f7c7a9970775b2014
StudioMaX <starosekpd@gmail.com>
2018-06-22 19:25:03 +0600

    14 commit

--- test.php
+++ test.php
@@ -1,8 +1,27 @@
 <?php
+
+use Ttt\ttt;
+
 echo 'test content';
 echo 'second line1111233111';
 echo 'second line1111233111 gfdggfdgdfg dgffdgdf gdf gdfgfdgfd gdf gfdg fd gfdg fdg fdg fd gdfg fdg fdg fg dfg fg dfg fdg fdg fdg fdg dfg dfgdf';
 
+/*
+ * fgh
+ * fgh
+ * gf
+ * h
+ * f
+ * h
+ * f
+ * gh
+ * fg
+ * h1
+ * fg
+ * h
+ * fg
+ * hfg
+ */
 /**
  * @param string $obj
  */
@@ -11,13 +30,18 @@ function TsuperFunc(Obj1 $obj){
 }
 $tt = new sdfasfdsffsdfsfsdfsd();
 $t = 5 / 0;
-hgfhfghgfhgf
-
+/*
+ * dff
+ * sdf
+ * s
+ * f
+ * s
+ * f
+ * s
+ * f
+ * s
+ * fd
+ */
 function NewFunc(){
 
 }
- sdgsdfs
-
- tertretreter
- gfhfghfghgf
-nbvnvbn
